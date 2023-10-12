7f64522741d72e94c8a43d774e971b8857be99be
kellyannclark <thefaithseed@gmail.com>
2023-10-12 13:30:49 -0600

    changes

--- controllers/book.js
+++ controllers/book.js
@@ -1,3 +1,10 @@
+
+const express = require("express");
+const router = express.Router();
+const mongodb = require("../data/database");
+const ObjectId = require("mongodb").ObjectId;
+
+
 const book1Route = (req, res) => {
     res.send(" Hello Book1");
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //express web server
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
 app.use('/', require('./routes/index'));
@@ -8,3 +9,13 @@ const port = 3000;
 
 app.listen(process.env.port || port);
 console.log("Web server is listening at port" + (process.env.port || port));
+
+mongodb.initDb((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port, () => {
+        console.log(`Database is listening, and the node is running on port ${port}`);
+      });
+    }
+  });
